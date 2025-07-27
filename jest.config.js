2f6ed4eb16d3db34e9a48df8dc0888dc0442b407
PedroRomaoDev <pedro7ntj@gmail.com>
2025-07-27 17:43:07 -0300

    chore: add jest setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'node'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/**/*.js'],
+
+  // 🧩 Adicione isso:
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+};
