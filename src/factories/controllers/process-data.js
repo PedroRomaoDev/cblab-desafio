f5d0d4c58f499fd332c370baa7de21a47ddec5a9
PedroRomaoDev <pedro7ntj@gmail.com>
2025-07-28 19:06:00 -0300

    feat: add makeProcessDataController factory

---
+++ src/factories/controllers/process-data.js
@@ -0,0 +1,24 @@
+// src/factories/controllers/process-data.js
+
+// Importa as classes necessárias para montar o controlador
+import { RawDataRepository } from '../../repositories/raw-zone.js';
+import { ProcessedDataRepository } from '../../repositories/processed-data.js';
+import { ProcessDataUseCase } from '../../usecases/process-data.js';
+import { ProcessDataController } from '../../controllers/process-data.js';
+
+export const makeProcessDataController = () => {
+  // 1. Instancia Repositórios
+  const rawDataRepository = new RawDataRepository();
+  const processedDataRepository = new ProcessedDataRepository();
+
+  // 2. Instancia Use Case, injetando Repositórios
+  const processDataUseCase = new ProcessDataUseCase(
+    rawDataRepository,
+    processedDataRepository,
+  );
+
+  // 3. Instancia Controller, injetando Use Case
+  const processDataController = new ProcessDataController(processDataUseCase);
+
+  return processDataController;
+};
